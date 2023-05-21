cff4e967296d3b5a638ce0cf52bdb46b617dbd14
Nicole <nicole33how@berkeley.edu>
2023-05-21 11:24:05 -0700

    Changes

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Welcome() {
+    return (
+    <div id='welcome-container'>
+        <h1 className='title-text'>Welcome</h1>
+        <h1 className='title-text'>Hello</h1>
+    </div>
+    
+    );
+}
+
+export default Welcome
